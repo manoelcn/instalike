11682be802bff60bb886fbeb2f3397e0bdda1112
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-23 11:53:18 -0300

    implementando armazenamento e upload de imgs

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,9 +1,16 @@
-import conectarAoBanco from '../config/dbConfig.js';
+import conectarAoBanco from "../config/dbConfig.js"
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 export async function getTodosPosts() {
-    const db = conexao.db('instalike');
-    const colecao = db.collection('posts');
+
+    const db = conexao.db("instalike");
+    const colecao = db.collection("posts");
     return colecao.find().toArray();
+};
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("instalike");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 };
