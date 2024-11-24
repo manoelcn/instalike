9730de92d042609bd62345341026ba278b4729ad
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-24 10:43:07 -0300

    configuração da api e integração com gemini

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,10 +1,12 @@
-import { getTodosPosts, criarPost } from "../models/postsModel.js";
 import fs from "fs";
+import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
+
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
-}
+};
 
 export async function postarNovoPost(req, res) {
     const novoPost = req.body;
@@ -15,7 +17,7 @@ export async function postarNovoPost(req, res) {
         console.error(erro.message);
         res.status(500).json({ "Erro": "Falha na requisição" });
     }
-}
+};
 
 export async function uploadImagem(req, res) {
     const novoPost = {
@@ -28,6 +30,26 @@ export async function uploadImagem(req, res) {
         const postCriado = await criarPost(novoPost);
         const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
         fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        res.status(500).json({ "Erro": "Falha na requisição" });
+    }
+};
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`;
+
+    try {
+        const imagemBuffer = fs.readFileSync(`uploads/${id}.png`);
+        const descricao = await gerarDescricaoComGemini(imagemBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        };
+        const postCriado = await atualizarPost(id, post);
         res.status(200).json(postCriado);
     } catch (erro) {
         console.error(erro.message);
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,9 +1,9 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 export async function getTodosPosts() {
-
     const db = conexao.db("instalike");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
@@ -13,4 +13,11 @@ export async function criarPost(novoPost) {
     const db = conexao.db("instalike");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost);
+};
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("instalike");
+    const colecao = db.collection("posts");
+    const objectId = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objectId)}, {$set: novoPost});
 };
